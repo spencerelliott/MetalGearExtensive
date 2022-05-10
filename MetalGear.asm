b2f2b60d1470008efef7317887843c23b7c1d860
Spencer Elliott <s.elliott123@gmail.com>
2022-05-10 15:04:09 -0500

    Bump ROM up to 256KB

--- MetalGear.asm
+++ MetalGear.asm
@@ -60,5 +60,11 @@ JAPANESE	equ	0	; 1 = Japanese version, 0 = English version
 		include	"Banks789.asm"
 		include	"BanksABC.asm"
 		include	"BanksDEF.asm"
+		include	"Banks10.asm"
+		include	"Banks11_12_13.asm"
+		include	"Banks14_15_16.asm"
+		include	"Banks17_18_19.asm"
+		include	"Banks1A_1B_1C.asm"
+		include	"Banks1D_1E_1F.asm"
 		
 		end
--- MetalGearBanks.asm
+++ MetalGearBanks.asm
@@ -65,5 +65,17 @@ JAPANESE	equ	0	; 1 = Japanese version, 0 = English version
 		include	"BanksABC.asm"
 		output "banks/BanksDEF.dat"
 		include	"BanksDEF.asm"
+		output "banks/Banks10.dat"
+		include "Banks10.asm"
+		output "banks/Banks11_12_13.dat"
+		include	"Banks11_12_13.asm"
+		output "banks/Banks14_15_16.dat"
+		include	"Banks14_15_16.asm"
+		output "banks/Banks17_18_19.dat"
+		include	"Banks17_18_19.asm"
+		output "banks/Banks1A_1B_1C.dat"
+		include	"Banks1A_1B_1C.asm"
+		output "banks/Banks1D_1E_1F.dat"
+		include	"Banks1D_1E_1F.asm"
 		
 		end
